6aeca29526c94f2bb7a101656758b1ddd41b7083
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2020-10-17 16:33:55 -0400

    (#66) ) relocate fixtures for use with tests, preventing inclusion of tests folder in prod build

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
-export { default as fixtures } from './fixtures';
 export { i18n } from './i18n';
 export { getProductTestBase } from './getProductTestBase';
 export { formatNumberToThousands } from './number';
